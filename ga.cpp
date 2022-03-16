68299b55a0d2164b3665171c0c1b8cf380446932
Soumik Dutta <shalearkane@gmail.com>
2022-03-16 11:15:18 +0530

    added chromosome struct

--- ga.cpp
+++ ga.cpp
@@ -1,4 +1,17 @@
+#define MAX_GENES 10
+
 typedef struct {
     int task;
     int processor;
 } gene;
+
+typedef struct {
+    gene genes[MAX_GENES];
+    int average_cost;
+    int makespan;
+} chromosome;
+
+int main() {
+    chromosome c;
+    return 0;
+}
