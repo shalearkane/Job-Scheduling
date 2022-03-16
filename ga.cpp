f225177e4b0832b741056d5959e5ebcbaa6ee784
PKM(Punit Kumar Meena) <punit.kumarmeena.cse20@itbhu.ac.in>
2022-03-16 20:52:02 +0530

    population

--- ga.cpp
+++ ga.cpp
@@ -7,22 +7,28 @@ struct gene
     int p;
 };
 
-
+int avg_cost(int t, vector<vector<pair<int,int>>>& chromosomes){
+    
+}
 
 int main(){
-    int n;cin>>n;
+    int t,p;cin>>t>>p;
+
+
     srand(time(0));
-    vector<vector<pair<int,int>>> chromosomes(n);
-    for(int j=0;j<n;j++){
+    vector<vector<pair<int,int>>> chromosomes(20);
+    for(int j=0;j<20;j++){
         for(int i=0;i<10;i++){
-            int t = ( rand() % 10 ) +1;
-            int p = ( rand() % 3 ) +1;
-            chromosomes[j][i].first = t;
-            chromosomes[j][i].second = p;
+            int a = ( rand() % 10 ) +1;
+            int b = ( rand() % 3 ) +1;
+
+            chromosomes[j].push_back({a,b});
         }
     }
-    
-    for(int j=0;j<n;j++){
+    avg_cost(t,chromosomes);
+    // problem - all tasks need to be unique
+
+    for(int j=0;j<20;j++){
         for(int i=0;i<10;i++){
             cout<<chromosomes[j][i].first<<" - "<<chromosomes[j][i].second<<"\n";
         }
